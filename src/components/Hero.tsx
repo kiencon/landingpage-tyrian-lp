cc50f67a6c7feb7c91d21fefdddedcc4272d9d20
VietSonBui <vietson1610@gmail.com>
2025-08-16 00:41:55 -0700

    update banner, remove images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 const Hero = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [displayLogo, setDisplayLogo] = useState(false);
   return (
     <div className='hero'>
       <div
@@ -13,10 +14,53 @@ const Hero = () => {
       <div className='container-full'>
         <section className='pt-48'>
           <div
-            onMouseEnter={() => setIsHovered(true)}
+            onMouseEnter={() => {
+              setIsHovered(true);
+              setDisplayLogo(true);
+            }}
             onMouseLeave={() => setIsHovered(false)}
+            className='relative flex justify-around'
           >
-            <img src='/tyrian-job-all-city.png' alt='logo' />
+            <div>
+              <img
+                className='block rounded-3xl'
+                width={300}
+                src={'/tyrian-job-ems.png'}
+                alt='tyrian-job-ems'
+              />
+            </div>
+            <div className='relative w-[300px]'>
+              <img
+                className='block rounded-3xl absolute w-full -top-[50px]'
+                width={300}
+                src={'/tyrian-job-PD.png'}
+                alt='tyrian-job-PD'
+              />
+            </div>
+            <div className='relative w-[300px]'>
+              <img
+                className='block rounded-3xl absolute w-full -top-[50px]'
+                width={300}
+                src={'/tyrian-job-crim.png'}
+                alt='tyrian-job-crim'
+              />
+            </div>
+            <div>
+              <img
+                className='block rounded-3xl'
+                width={300}
+                src={'/tyrian-job-worker.png'}
+                alt='tyrian-job-worker'
+              />
+            </div>
+          </div>
+          <div
+            style={{ transition: '1s', bottom: `${displayLogo ? '0' : ''}` }}
+            className={`absolute -bottom-[30px] left-[50%] -translate-x-[50%] ease-in ${
+              displayLogo ? '' : 'opacity-0'
+            }`}
+          >
+            <img src='/logo.webp' alt='logo' width={300} height={'auto'} />
           </div>
         </section>
       </div>
